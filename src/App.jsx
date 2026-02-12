52e273f11e0fa23b6c87a4726f8f2d7972a2bcd2
Adrito Rafsan <116940100+adritorafsan@users.noreply.github.com>
2026-02-12 14:56:25 +0600

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { Route, Routes } from "react-router"
+import Home from "./Pages/Home"
+import CheckOut from "./Pages/CheckOut"
+import AuthPage from "./Pages/AuthPage"
+import Navbar from "./Components/Navbar"
 
 function App() {
 
 
   return (
-    <>
-      <h1 className=" text-3xl text-red-500">Adrito Rafsan</h1>
-    </>
+    
+      <div>
+        <Navbar />
+
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<CheckOut />} />
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </div>
+      
+    
   )
 }
 
